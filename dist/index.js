012a08868ea4b852caf7034cc902604d85bf3193
Lucas Coelho <lucascoelhodacosta@gmail.com>
2022-08-17 21:04:08 -0300

    chore: test ls

--- dist/index.js
+++ dist/index.js
@@ -4183,12 +4183,12 @@ async function assembleSteamContentsFor(preset, buildDir) {
     if (preset.platform === _constants__WEBPACK_IMPORTED_MODULE_4__/* .DESKTOP_PLATFORMS.macOS */ .ub.macOS) {
         const buildLocation = buildDir.replace(path__WEBPACK_IMPORTED_MODULE_0___default().basename(preset.export_path), '');
         const resolvedBuildDir = buildDir.replace('.zip', '.app');
-        const macOSPath = path__WEBPACK_IMPORTED_MODULE_0___default().join(resolvedBuildDir, 'Contents', 'MacOS');
+        // const macOSPath = path.join(resolvedBuildDir, 'Contents', 'MacOS');
         await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('unzip', ['-q', buildDir, '-d', buildLocation]);
         await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('rm', [buildDir]);
-        await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('ls', [resolvedBuildDir]);
-        await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('cp', [_constants__WEBPACK_IMPORTED_MODULE_4__/* .STEAM_APPID_PATH */ .xX, macOSPath]);
-        await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('mv', [libPath, macOSPath]);
+        // await exec('ls', [resolvedBuildDir]);
+        // await exec('cp', [STEAM_APPID_PATH, macOSPath]);
+        // await exec('mv', [libPath, macOSPath]);
         await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec)('7z', ['a', buildDir, resolvedBuildDir]);
     }
     else {
